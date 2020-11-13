89196aee5a86893f353cf22aee446e5098de260d
laurennelson <laurenm.nelson@calbaptist.edu>
2020-11-13 08:34:36 -0800

    Fixed bug in EmployeeSchedule.py

--- EmployeeSchedule.py
+++ EmployeeSchedule.py
@@ -25,7 +25,7 @@ class Schedule:
 
             start_time = datetime.time(hour=start_hr, minute=start_min, second=0, microsecond=0)
             end_time = datetime.time(hour=end_hr, minute=end_min, second=0, microsecond=0)
-            new_employee = Employee(name, start_time, end_time)
+            new_employee = Employee(name, start_time, end_time, 0, 0, 0)
             array.append(new_employee)
 
         array.sort()
@@ -47,22 +47,35 @@ class Schedule:
             date_time_difference_in_hours = date_time_difference.total_seconds() / 3600
 
             if date_time_difference_in_hours >= 6:
-                first_break = time_start
-                print(first_break)
-
+                x.first_break = time_start + timedelta(hours=2)
+                x.lunch = x.first_break + timedelta(hours=2)
+                x.second_break = x.lunch + timedelta(hours=2)
+            else:
+                x.first_break = time_start + timedelta(hours=2)
+                x.lunch = datetime.time(hour=0, minute=0, second=0, microsecond=0)
+                x.second_break = datetime.time(hour=0, minute=0, second=0, microsecond=0)
         return
 
     # Prints out the people in order
-    def print_in_order(self, unsorted_array):
-        for x in range(len(unsorted_array)):
-            print(unsorted_array[x].start_time.strftime("%I:%M%p").lstrip("0").lower(),
-                  unsorted_array[x].end_time.strftime("%I:%M%p").lstrip("0").lower())
+    def print_in_order(self, array_sorted):
+        for x in range(len(array_sorted)):
+            if array_sorted[x].lunch != datetime.time(hour=0, minute=0, second=0, microsecond=0):
+                print(array_sorted[x].name, array_sorted[x].start_time.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].first_break.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].lunch.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].second_break.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].end_time.strftime("%I:%M%p").lstrip("0").lower())
+            else:  # no lunch
+                print(array_sorted[x].name, array_sorted[x].start_time.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].first_break.strftime("%I:%M%p").lstrip("0").lower(),
+                      array_sorted[x].end_time.strftime("%I:%M%p").lstrip("0").lower())
 
 
 def main():
     my_schedule = Schedule()
     sorted_array = my_schedule.read_file()
-    calculated_breaks = my_schedule.calculate_breaks(sorted_array)
+    my_schedule.calculate_breaks(sorted_array)
+    my_schedule.print_in_order(sorted_array)
 
 
 if __name__ == "__main__":
